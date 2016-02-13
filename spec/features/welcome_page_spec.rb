4267a9d54be2cbfc2c61e782d3067ffa1044f920
Vitaly Tatarintsev <Kalastiuz@gmail.com>
2016-02-13 16:44:23 +0100

    Configure top navbar

--- spec/features/welcome_page_spec.rb
+++ spec/features/welcome_page_spec.rb
@@ -4,6 +4,8 @@ feature "Welcome page" do
   scenario "everybody can see title of the project" do
     visit root_path
 
-    expect(page).to have_content "Rep of Spaces"
+    within "nav.navbar" do
+      expect(page).to have_content "Rep of Spaces"
+    end
   end
 end
