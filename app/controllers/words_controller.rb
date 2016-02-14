09dd7f6106d6d9eb551084813982430e47326da6
Vitaly Tatarintsev <Kalastiuz@gmail.com>
2016-02-14 11:19:24 +0100

    Allow to create new words

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -8,6 +8,19 @@ class WordsController < ApplicationController
   def show
   end
 
+  def new
+    @word = Word.new
+  end
+
+  def create
+    @word = current_user.words.new safe_params
+    if @word.save
+      redirect_to words_path, notice: "You have added one more word"
+    else
+      render :new
+    end
+  end
+
   def destroy
     @word.destroy
     redirect_to words_url
@@ -17,5 +30,9 @@ class WordsController < ApplicationController
 
   def find_word
     @word = Word.find params[:id]
+  end
+
+  def safe_params
+    params.require(:word).permit(:content, :translation, :excerpt)
   end
 end
---
+++ spec/features/create_new_word_spec.rb
@@ -0,0 +1,24 @@
+require "rails_helper"
+
+feature "Add new Word" do
+  given!(:user) { create :user_example_com }
+
+  scenario "user can add his own words" do
+    sign_in_as "user@example.com"
+    visit words_path
+    click_link "+ Add"
+
+    within "#new_word" do
+      fill_in "word_content", with: "Wasser"
+      fill_in "word_translation", with: "Water"
+      fill_in "word_excerpt", with: "Ich trinke Wasser"
+      click_button "Add"
+    end
+
+    expect(page).to have_content "You have added one more word"
+    within "#words" do
+      expect(page).to have_content "Wasser"
+    end
+    expect(Word.count).to eq 1
+  end
+end
