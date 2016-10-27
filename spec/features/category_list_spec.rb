a33b17d547cf3cf21fbbd690901fe4bb6650413f
Vitali Tatarintev <Kalastiuz@gmail.com>
2016-10-27 15:57:58 +0200

    Add no categories yet explanation text

--- spec/features/category_list_spec.rb
+++ spec/features/category_list_spec.rb
@@ -3,21 +3,38 @@ require 'rails_helper'
 feature "List of categories" do
   given!(:user) { create :user_example_com }
   given!(:word) { create :word, content: "Wasser", user: user }
-  given!(:category) { create :category, name: "Getränke", user: user }
 
-  background do
-    word.categories << category
-  end
+  context "when user have categories" do
+    given!(:category) { create :category, name: "Getränke", user: user }
 
-  scenario "can see list of categories" do
-    sign_in_as "user@example.com"
-
-    within "nav.navbar" do
-      click_link "Categories"
+    background do
+      word.categories << category
     end
 
-    within "#categories" do
-      expect(page).to have_selector "tr", text: "Getränke (1 Word)"
+    scenario "can see list of categories" do
+      sign_in_as "user@example.com"
+
+      within "nav.navbar" do
+        click_link "Categories"
+      end
+
+      within "#categories" do
+        expect(page).to have_selector "tr", text: "Getränke (1 Word)"
+      end
+    end
+  end
+
+  context "when user have no categories" do
+    scenario "user sees explanation message instread of table" do
+      sign_in_as "user@example.com"
+
+      within "nav.navbar" do
+        click_link "Categories"
+      end
+
+      within "#categories" do
+        expect(page).to have_content "You have no categories yet. You can add them from the create/update word page."
+      end
     end
   end
 end
