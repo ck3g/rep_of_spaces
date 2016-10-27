254d56db4031e0e5890c4b6998c4707aea7771ef
Vitali Tatarintev <Kalastiuz@gmail.com>
2016-10-27 14:32:14 +0200

    Display list of available categories

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :words
+  resources :categories, only: [:index]
   resource :practice, only: [:new, :create]
 
   # Serve websocket cable requests in-process
